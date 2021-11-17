29334816dec66eb372cd7fe3b9a8e6911d03c76c
sundalei <sundalei1988@gmail.com>
2021-11-17 22:39:17 +0800

    Payroll

--- taco-cloud/src/main/java/tacos/web/api/IngredientController.java
+++ taco-cloud/src/main/java/tacos/web/api/IngredientController.java
@@ -1,13 +1,34 @@
 package tacos.web.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tacos.Ingredient;
+import tacos.data.IngredientRepository;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "ingredients", produces = MediaType.APPLICATION_JSON_VALUE)
 @CrossOrigin("*")
 public class IngredientController {
 
+    private final IngredientRepository ingredientRepo;
+
+    @Autowired
+    public IngredientController(IngredientRepository ingredientRepo) {
+        this.ingredientRepo = ingredientRepo;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Ingredient> getIngredientById(@PathVariable("id") String id) {
+        Optional<Ingredient> optional = ingredientRepo.findById(id);
+        if (optional.isPresent()) {
+            return new ResponseEntity<>(optional.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
 }
