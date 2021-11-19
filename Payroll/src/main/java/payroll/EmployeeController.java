7f400a59759f8364722380f99540f18b7ab52148
DaLei Sun <dalei.sun@infor.com>
2021-11-19 11:40:02 +0800

    add assembler

--- Payroll/src/main/java/payroll/EmployeeController.java
+++ Payroll/src/main/java/payroll/EmployeeController.java
@@ -5,7 +5,6 @@ import java.util.stream.Collectors;
 
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,17 +21,18 @@ class EmployeeController {
     
     private final EmployeeRepository repository;
 
-    EmployeeController(EmployeeRepository repository) {
+    private final EmployeeModelAssembler assembler;
+
+    EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler) {
         this.repository = repository;
+        this.assembler = assembler;
     }
 
     @GetMapping("/employees")
     CollectionModel<EntityModel<Employee>> all() {
         List<EntityModel<Employee>> employees = repository.findAll().stream()
-            .map(employee -> EntityModel.of(employee,
-                linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
-                linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
-                .collect(Collectors.toList());
+            .map(assembler::toModel)
+            .collect(Collectors.toList());
         
         return CollectionModel.of(employees,
             linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
@@ -48,9 +48,7 @@ class EmployeeController {
         Employee employee = repository.findById(id)
           .orElseThrow(() -> new EmployeeNotFoundException(id));
         
-        return EntityModel.of(employee,
-            linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
-            linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+        return assembler.toModel(employee);
     }
 
     @PutMapping("/employees/{id}")
