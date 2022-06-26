45db98891edc2c820238f87732e19e6db37157c1
Leo Sun <leosun@Leos-MacBook-Pro.local>
2022-06-26 23:54:09 +0800

    test

--- learning-springboot/src/main/java/com/demo/learning/courses/controller/CourseController.java
+++ learning-springboot/src/main/java/com/demo/learning/courses/controller/CourseController.java
@@ -13,6 +13,12 @@ public class CourseController {
     
     @GetMapping("/courses")
     public List<Course> getAllCourses() {
-        return Arrays.asList(new Course(1, "Learn Microservices", "in28minutes"));
+        return Arrays.asList(new Course(1, "Learn Microservices", "in28minutes"),
+        		new Course(2, "Learn Full Stack with Angular and React", "in28minutes"));
+    }
+    
+    @GetMapping("/courses/1")
+    public Course getCourseDetails() {
+    	return new Course(1, "Learn Microservices", "in28minutes");
     }
 }
