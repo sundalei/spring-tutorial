430a908a728bcfd7178348208c5c12d4ce021db0
Leo Sun <leosun@Leos-MacBook-Pro.local>
2022-06-26 19:26:02 +0800

    test

---
+++ learning-springboot/src/main/java/com/demo/learning/courses/bean/Course.java
@@ -0,0 +1,30 @@
+package com.demo.learning.courses.bean;
+
+public class Course {
+    private long id;
+    private String name;
+    private String author;
+
+    public Course(long id, String name, String author) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Course [author=" + author + ", id=" + id + ", name=" + name + "]";
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
